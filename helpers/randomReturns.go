2178f59107a65abca94d5b5fcee671ee2da54400
Stefan Moravcik <stefan@virsas.com>
2022-04-19 10:40:01 +0200

    added func for random int and random public key

--- helpers/randomReturns.go
+++ helpers/randomReturns.go
@@ -19,3 +19,23 @@ func RandomString(size int, prefix string) string {
 
 	return randomString
 }
+
+func RandomInt64(min int64, max int64) int64 {
+	rand.Seed(time.Now().UnixNano())
+
+	value := rand.Int63n(max-min+1) + min
+	return value
+}
+
+func RandomPubString(length int, size int) string {
+	var finalString string
+
+	for i := 1; i <= size; i++ {
+		finalString = finalString + RandomString(length, "")
+		if i < size {
+			finalString = finalString + "-"
+		}
+	}
+
+	return finalString
+}
